12aa3dea10cd17cd4857099f0796a0cd79b9bf5e
Akiji Yamamoto <ayamamotojpn@ybb.ne.jp>
2022-05-31 17:56:16 +0900

    installgnu.sh install_pgi.sh modified

--- install_gnu.sh
+++ install_gnu.sh
@@ -1,3 +1,3 @@
-cp libglapack_gpu.so /usr/local/glapack/lib/gnu
+cp libglapack_cpu.so /usr/local/glapack/lib/gnu
 cp glapack.mod /usr/local/glapack/include/gnu
-cp libgblas_gpu.so /usr/local/glapack/lib/gnu
+cp libgblas_cpu.so /usr/local/glapack/lib/gnu
--- install_pgi.sh
+++ install_pgi.sh
@@ -1,3 +1,3 @@
-cp libglapack_gpu.so /usr/local/glapack/lib/pgi
+cp libglapack_cpu.so /usr/local/glapack/lib/pgi
 cp glapack.mod /usr/local/glapack/include/pgi
-cp libgblas_gpu.so /usr/local/glapack/lib/pgi
+cp libgblas_cpu.so /usr/local/glapack/lib/pgi
