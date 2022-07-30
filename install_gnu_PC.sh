7b6cf9eb1f96db8f904b1d76b08a4611806fc6da
Akiji Yamamoto <ayamamotojpn@ybb.ne.jp>
2022-07-30 13:53:40 +0900

    install_gnu_PC.sh install_intel_PC.sh install_nvidia_PC.sh added

---
+++ install_gnu_PC.sh
@@ -0,0 +1,4 @@
+cp libglapack_gpu.so /usr/local/lib/gnu
+cp obj_86/glapack.mod /usr/local/include/gnu
+cp libgblas_gpu.so /usr/local/lib/gnu
+cp obj_86/cublasxt.mod /usr/local/include/gnu
---
+++ install_intel_PC.sh
@@ -0,0 +1,4 @@
+cp libglapack_gpu.so /usr/local/lib/intel
+cp obj_86/glapack.mod /usr/local/include/intel
+cp libgblas_gpu.so /usr/local/lib/intel
+cp obj_86/cublasxt.mod /usr/local/include/intel
