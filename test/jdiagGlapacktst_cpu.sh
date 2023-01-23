8c617c2cf5616d432e3e60a09fd4c14d6f769627
Akiji Yamamoto <ayamamotojpn@ybb.ne.jp>
2023-01-23 13:22:01 +0900

    jdiagGlapacktst_cpu.sh jdiagGlapacktst_gpu.sh jdiagMagmatst.sh modified

--- test/jdiagGlapacktst_cpu.sh
+++ test/jdiagGlapacktst_cpu.sh
@@ -8,7 +8,9 @@ CUDAVER=11.8
 #CUDAVER=12.0
 CUDALIB=/home/akiji/local/cuda-$CUDAVER/targets/x86_64-linux/lib
 MKLLIB=/opt/intel/oneapi/mkl/$MKLVER/lib/intel64
-export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib/$INSD:$MKLLIB:$CUDALIB:$LD_LIBRARY_PATH
+OMPLIB=/opt/intel/oneapi/compiler/$MKLVER/linux/compiler/lib/intel64_lin
+GLAPACK=../lib
+export LD_LIBRARY_PATH=$GLAPACK/usr/local/lib:/usr/local/lib/$INSD:$MKLLIB:$OMPLIB:$LD_LIBRARY_PATH
 
 jdiagGlapacktst_cpu 10
 
--- test/jdiagMagmatst.sh
+++ test/jdiagMagmatst.sh
@@ -1,16 +1,15 @@
 #for gfortran
-#INSD=gnu
-
-#for ifort
-INSD=intel
-
+INSD=gnu
 MKLVER=2022.1.0
 CUDAVER=11.8
 #CUDAVER=12.0
-CUDALIB=$HOME/local/cuda-$CUDAVER/targets/x86_64-linux/lib
+CUDALIB=/home/akiji/local/cuda-$CUDAVER/targets/x86_64-linux/lib
 MKLLIB=/opt/intel/oneapi/mkl/$MKLVER/lib/intel64
 GLAPACK=../lib
 export LD_LIBRARY_PATH=$GLAPACK:/usr/local/lib:/usr/local/lib/$INSD:$MKLLIB:$CUDALIB:$LD_LIBRARY_PATH
 
-jdiagGlapacktst_gpu 10
+jdiagMagmatst 10 1
+
+jdiagMagmatst 10 2
+
 
