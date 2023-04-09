8d5c3373a5acb6f74eb2873c1945bf9bb86f06d7
Akiji Yamamoto <ayamamotojpn@ybb.ne.jp>
2023-04-09 17:58:02 +0900

    test/jdiagGlapacktst.f90 test/jdiagMagmatst.f90 modified

--- test/jdiagGlapacktst.f90
+++ test/jdiagGlapacktst.f90
@@ -4,7 +4,11 @@ program jdiagGlapacktst
   use magma
 #endif
   implicit none
-  real(8),allocatable :: a(:,:),w(:)
+  real(8),allocatable :: az(:,:),wz(:)
+  real,allocatable :: ac(:,:),wc(:)
+  real(8),allocatable :: ad(:,:),wd(:)
+  real,allocatable :: as(:,:),ws(:)
+
   integer :: iarg,ngpu,n,lda,i,j
   character(10) :: arg1,arg2
 
@@ -19,29 +23,37 @@ program jdiagGlapacktst
   read(arg2,*) ngpu
   lda=n
 
-  allocate(a(n,n),w(n))
+  allocate(as(n,n),ws(n))
+  allocate(ad(n,n),wd(n))
+  allocate(ac(n,n),wc(n))
+  allocate(az(n,n),wz(n))
 
   do i=1,n
     do j=1,i
-      a(i,j)=i+1
-      a(j,i)=a(i,j)
+      as(i,j)=i+1
+      as(j,i)=as(i,j)
     end do
   end do
 
-  write(6,*) 'a'
+  ad(:,:)=as(:,:)
+  ac(:,:)=as(:,:)
+  az(:,:)=ac(:,:)
+
+
+  write(6,*) 'as'
   do i=1,n
-    write(6,'(20f10.4)') a(i,:)
+    write(6,'(20f10.4)') as(i,:)
   end do
 
-  if(ngpu>1) then
+  if(ngpu==1) then
     call glapack_init()
   else
     call glapack_init(ngpu)
   end if
-  call jdiagGlapack(lda,n,A,W)
+  call jdiagGlapackd(lda,n,ad,wd)
 
-  write(6,*) 'w'
-  write(6,'(20f10.4)') w(:)
+  write(6,*) 'wd'
+  write(6,'(20f10.4)') wd(:)
 
   stop
 
@@ -54,7 +66,7 @@ contains
     stop
   end subroutine
 
-  subroutine jdiagGlapack(lda,n,A,W)
+  subroutine jdiagGlapackd(lda,n,A,W)
     use glapack
     ! A: input matrix, and eigenvectors will be stored here
     ! W: vector for eigenvalues
@@ -76,24 +88,24 @@ contains
     integer :: status
 
     call glapack_ev_prm('V', 'L', n, A, lda, lwork, lrwork, liwork) ! error occures here
-#ifdef MAGMA
-    !call magmaf_dsyevd_m (ngpu, 'V', 'L', n, A, lda, W, WORK, lwork, IWORK, liwork, info)
-#else
-    !call dsyev('V', 'L', n, A, lda, lwork, lrwork, liwork)
-#endif
+!#ifdef MAGMA
+!    !call magmaf_dsyevd_m (ngpu, 'V', 'L', n, A, lda, W, WORK, lwork, IWORK, liwork, info)
+!#else
+!    !call dsyev('V', 'L', n, A, lda, lwork, lrwork, liwork)
+!#endif
     write(6,'(a,3i10)') 'lwork,lrwork,liwork=',lwork,lrwork,liwork ! for test
     ! allocate with the new, proper size:
     allocate (work(lwork), IWORK(liwork))        !, stat=status)
     call glapack_ev('V', 'L', n, A, lda, W, WORK, lwork, IWORK, liwork, info)
-#ifdef MAGMA
-    !call magmaf_dsyevd_m (ngpu, 'V', 'L', n, A, lda, W, WORK, lwork, IWORK, liwork, info)
-#else
-    !call dsyev('V', 'L', n, A, lda, lwork, lrwork, liwork)
-#endif
+!#ifdef MAGMA
+!    !call magmaf_dsyevd_m (ngpu, 'V', 'L', n, A, lda, W, WORK, lwork, IWORK, liwork, info)
+!#else
+!    !call dsyev('V', 'L', n, A, lda, lwork, lrwork, liwork)
+!#endif
 
-#ifdef MAGMA 
-    call magmaf_finalize()  !only for gpu version
-#endif 
+!#ifdef MAGMA 
+!    call magmaf_finalize()  !only for gpu version
+!call magmaf_init() #endif 
     return
   end
 
--- test/jdiagMagmatst.f90
+++ test/jdiagMagmatst.f90
@@ -1,6 +1,10 @@
 program jdiagMagmatst
 implicit none
-real(8),allocatable :: a(:,:),w(:)
+real(8),allocatable :: ad(:,:),wd(:)
+real,allocatable :: as(:,:),ws(:)
+complex(8),allocatable :: az(:,:),wz(:)
+complex,allocatable :: ac(:,:),wc(:)
+
 integer :: iarg,n,ngpu,lda
 integer :: i,j
 character(10) :: arg1,arg2
@@ -16,25 +20,33 @@ read(arg1,*) n
 read(arg2,*) ngpu
 lda=n
 
-allocate(a(n,n),w(n))
+allocate(as(n,n),ws(n))
+allocate(ad(n,n),wd(n))
+allocate(ac(n,n),wc(n))
+allocate(az(n,n),wz(n))
 
 do i=1,n
   do j=1,i
-    a(i,j)=i+1
-    a(j,i)=a(i,j)
+    as(i,j)=i+1
+    as(j,i)=as(i,j)
   end do
 end do
 
-write(6,*) 'a'
+ad(:,:)=as(:,:)
+ac(:,:)=as(:,:)
+az(:,:)=ac(:,:)
+
+write(6,*) 'as'
 do i=1,n
-  write(6,'(20f10.4)') a(i,:)
+  write(6,'(20f10.4)') as(i,:)
 end do
 
 call magmaf_init() 
-call jdiagMAGMA(ngpu,lda,n,A,W)
 
-write(6,*) 'w'
-write(6,'(20f10.4)') w(:)
+call jdiagMAGMAd(ngpu,lda,n,ad,wd)
+
+write(6,*) 'wd'
+write(6,'(20f10.4)') wd(:)
 
 stop
 
@@ -47,7 +59,7 @@ subroutine usage()
   stop
 end subroutine
 
-subroutine jdiagMAGMA(ngpu,lda,n,A,W) 
+subroutine jdiagMAGMAd(ngpu,lda,n,A,W) 
 ! A: input matrix, and eigenvectors will be stored here
 ! W: vector for eigenvalues
 ! n: matrix size   (4*NCC, 4*az atomok szama)
