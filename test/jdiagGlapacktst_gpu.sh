2092417f3c7939d79909e60b9221131b0016f85a
Akiji Yamamoto <ayamamotojpn@ybb.ne.jp>
2023-04-10 09:21:57 +0900

    jdiagGlapacktst_gpu.sh jdiagMagmatst.sh mk_jdiagGlapack_cpu mk_jdiagGlapack_gpu in test modified

--- test/jdiagGlapacktst_gpu.sh
+++ test/jdiagGlapacktst_gpu.sh
@@ -6,16 +6,16 @@ INSD=intel
 MKLVER=2022.1.0
 #CUDAVER=11.8
 #MKLVER=2023.0.0
-CUDAVER=12.0
-CUDALIB=$HOME/local/cuda-$CUDAVER/targets/x86_64-linux/lib
-MKLLIB=/opt/intel/oneapi/mkl/$MKLVER/lib/intel64
+export CUDAVER=12.0
+export CUDALIB=$HOME/local/cuda-$CUDAVER/targets/x86_64-linux/lib
+export MKLLIB=/opt/intel/oneapi/mkl/$MKLVER/lib/intel64
 #GLAPACK=../lib
-GLAPACK=/usr/local/lib/$INSD
+export GLAPACK=/usr/local/lib/$INSD
 export LD_LIBRARY_PATH=$GLAPACK:/usr/local/lib:/usr/local/lib/$INSD:$MKLLIB:$CUDALIB:$LD_LIBRARY_PATH
 
-jdiagGlapacktst_gpu 10 1
+./jdiagGlapacktst_gpu 10 1
 
-jdiagGlapacktst_gpu 10 2
+#./jdiagGlapacktst_gpu 10 2
 
 
 # at the moment intel version does not work
--- test/jdiagMagmatst.sh
+++ test/jdiagMagmatst.sh
@@ -1,12 +1,16 @@
 #for gfortran
-INSD=gnu
+#INSD=gnu
+#for ifort
+INSD=intel
+
 MKLVER=2022.1.0
-CUDAVER=11.8
-#CUDAVER=12.0
+#CUDAVER=11.8
+CUDAVER=12.0
 CUDALIB=/home/akiji/local/cuda-$CUDAVER/targets/x86_64-linux/lib
 MKLLIB=/opt/intel/oneapi/mkl/$MKLVER/lib/intel64
+MAGMALIB = /usr/local/magma-2.7.0_intel/lib
 GLAPACK=../lib
-export LD_LIBRARY_PATH=$GLAPACK:/usr/local/lib:/usr/local/lib/$INSD:$MKLLIB:$CUDALIB:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$GLAPACK:/usr/local/lib:/usr/local/lib/$INSD:$MKLLIB:$CUDALIB:$MAGMALIB:$LD_LIBRARY_PATH
 
 jdiagMagmatst 10 1
 
